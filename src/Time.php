a503ea7e1202e48efa643c76e87a43b27bd81219
Łukasz Radziak <lukasilestam@gmail.com>
2021-07-21 17:03:36 +0200

    Init field

---
+++ src/Time.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Lukasilestam\Time;
+
+use Laravel\Nova\Fields\Field;
+
+class Time extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'time';
+}
